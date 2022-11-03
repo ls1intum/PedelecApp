13ef473cf6910f7d6c4b61391a6428a47a59ec6e
Julian Benjamin Kretzschmar <julian.kretzschmar@tum.de>
2022-11-03 16:02:19 +0100

    Add initial Hello World Xcode project to repository

---
+++ Pedelec/Pedelec/PedelecApp.swift
@@ -0,0 +1,17 @@
+//
+//  PedelecApp.swift
+//  Pedelec
+//
+//  Created by Julian Kretzschmar on 03.11.22.
+//
+
+import SwiftUI
+
+@main
+struct PedelecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
